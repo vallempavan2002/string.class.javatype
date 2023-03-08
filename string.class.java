dfc39987da5faf713dcb54592c607b1d18373911
Pavan kalyan Vallem <116752882+vallempavan2002@users.noreply.github.com>
2023-03-08 11:54:18 +0530

    Create string.class.java

---
+++ string.class.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String []args)
+    {
+        String s1="QISCET";
+        String s2="eee";
+        System.out.println(s1+s2);
+        System.out.println(s1.concat(s2));
+        System.out.println("hello".concat("pk"));
+        System.out.println("%s%s",(s2,s1));
+        System.out.println("%S%S",(s2,s1));
+        System.out.println("---",(s1,s2));
+    }
+}
